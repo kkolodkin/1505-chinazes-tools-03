767392f4a3c9561ea71aa989495dcbebb8238f44
flandy67 <ufandy903@gmail.com>
2026-03-22 17:57:53 +0300

    fourth task

---
+++ problem4.py
@@ -0,0 +1,18 @@
+import random
+
+while True:
+    s = ['камень', 'ножницы', 'бумага']
+    choice1 = input('Выбери: камень, ножницы, бумага: ')
+    choice1 = choice1.lower()
+    if choice1 in s:
+        choice2 = random.choice(s)
+        if (choice1 == 'камень' and choice2 == 'ножницы') or (choice1 == 'бумага' and choice2 == 'камень') or (choice1 == 'ножницы' and choice2 == 'бумага'):
+            print('Ты выиграл')
+        elif choice1 == choice2:
+            print('Ничья')
+        else:
+            print('Я победил')
+    else:
+        print('Ошибка')
+        
+    
