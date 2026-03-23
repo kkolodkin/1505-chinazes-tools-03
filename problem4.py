1c006fbf4773eca45f78ceb623a0eb163a291e18
6661763 <you@example.com>
2026-03-23 10:41:06 +0300

    добавил вывод выбора компьютера

--- problem4.py
+++ problem4.py
@@ -7,10 +7,13 @@ while True:
     if choice1 in s:
         choice2 = random.choice(s)
         if (choice1 == 'камень' and choice2 == 'ножницы') or (choice1 == 'бумага' and choice2 == 'камень') or (choice1 == 'ножницы' and choice2 == 'бумага'):
+            print('Мой выбор: ' + choice2)
             print('Ты выиграл')
         elif choice1 == choice2:
+            print('Мой выбор: ' + choice2)
             print('Ничья')
         else:
+            print('Мой выбор: ' + choice2)
             print('Я победил')
     else:
         print('Ошибка')
