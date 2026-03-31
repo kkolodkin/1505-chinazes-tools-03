5346acde7a311c1deda2cdfe151da7c56d1cfc06
uququp <chiston.jr@gmail.com>
2026-03-31 12:42:10 +0300

    Delete task9.py

--- task9.py
+++
@@ -1,5 +0,0 @@
-import requests
-q = requests.get("https://api.open-meteo.com/v1/forecast?latitude=55.7558&longitude=37.6176&current_weather=true")
-wea = q.json()
-curtemp = wea['current_weather']['temperature']
-print(f'Ceйчас в Москве: {curtemp} градусов по цельсию')
